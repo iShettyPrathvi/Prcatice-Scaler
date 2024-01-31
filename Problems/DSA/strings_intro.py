dc0a52ddbea678fce24515edcac7a972ec3bce50
iShettyPrathvi <36467953+iShettyPrathvi@users.noreply.github.com>
2024-02-01 00:09:25 +0530

    rearranged

--- Problems/DSA/strings_intro.py
+++ Problems/DSA/strings_intro.py
@@ -53,14 +53,76 @@ class Solution:
         print(char_list)
         print(len(char_list))
         return ''.join(char_list)
+    
+
+    '''Akash likes playing with strings. One day he thought of applying following operations on the string in the given order:
+    Concatenate the string with itself.
+    Delete all the uppercase letters.
+    Replace each vowel with '#'.
+    You are given a string A of size N consisting of lowercase and uppercase alphabets. Return the resultant string after applying the above operations.
+    NOTE: 'a' , 'e' , 'i' , 'o' , 'u' are defined as vowels.'''
+    def string_modify(self, A):
+        n = len(A)
+        char_list = list(A)
+
+        #concatenate the string with itself
+        char_list += char_list
+
+        #delete all the uppercase letters
+        char_list = [c for c in char_list if c.islower()]
+
+        #replace each vowel with '#'
+        for idx in range(len(char_list)):
+            if char_list[idx] in ['a','e','i','o','u']:
+                char_list[idx] = '#'
+        
+        return ''.join(char_list)
+    
+    def longestCommonPrefix(self, strings_array):
+        if not A:
+            return ""
+
+        # Take the first string as the initial prefix
+        prefix = A[0]
+
+        # Iterate through the array of strings
+        for i in range(1, len(A)):
+            j = 0
+
+            # Compare characters of the current string with the prefix
+            while j < len(prefix) and j < len(A[i]) and prefix[j] == A[i][j]:
+                j += 1
+
+            # Update the prefix based on the common characters
+            prefix = prefix[:j]
+
+            # If the prefix becomes empty, no common prefix exists
+            if not prefix:
+                break
+
+        return prefix
+    
+    '''You are given a function isalpha() consisting of a character array A.
+    Return 1 if all the characters of a character array are alphanumeric (a-z, A-Z, and 0-9) else, return 0.'''
+    def isAlphNum(self, A):
+        n = len(A)
+        for i in range(n):
+            ch = A[i]
+            isalpha =  (ch>= '0' and ch<='9') or (ch>='A' and ch <='Z') or (ch >='a' and ch <= 'z')
+            if not isalpha:
+                return 0
+
+        return 1
 
 
 
 
 sol = Solution()
 
-A = 'crulgzfkif gg ombt vemmoxrgf qoddptokkz op xdq hv'
+A = ' crulgzfkif gg ombt vemmoxrgf qoddptokkz op xdq hv'
 res = sol.word_reverse(A)
 print(res)
 
-
+A = ['S','c','a','l','e','r','A','c','a','d','e','m','y','2','0','2','0']
+res = sol.isAlphNum(A)
+print(res)
